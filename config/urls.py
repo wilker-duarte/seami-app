b52ac20eba82fa289b039aa0b3dc8a9a26630594
wilker-duarte <wilkerduarte2002@gmail.com>
2026-08-19 23:06:33 -0300

    fix(auth): interceptar logout do admin e redirecionar para tela de login oficial do SEAMI

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts.views import logout_view
 from presencas.views import lista_alunos_view, lista_turmas_view, caderno_seami_view
 from core.views import relatorios_view, central_exportacao_view
 
 urlpatterns = [
+    # Intercepta qualquer logout (inclusive do Django Admin) para direcionar para o login do SEAMI
+    path('admin/logout/', logout_view, name='admin_logout'),
+    path('logout/', logout_view, name='root_logout'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('alunos/', lista_alunos_view, name='alunos'),
